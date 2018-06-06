75eccf5251c4e651e5a2c29ba22e8bf532b44b0e
Jonathan <Jonathan.Harris@mdpweb.net>
2018-06-05 23:18:23 -0400

    add simple validation

--- js/app.js
+++ js/app.js
@@ -7,6 +7,39 @@ document.addEventListener("DOMContentLoaded", function() {
         const days = document.getElementById('days').value;
         const locale = document.getElementById('locale').value;
 
+        let error = document.getElementById('datePickerError');
+
+        if(!date) {
+
+            error.classList.add('active');
+            return;
+        }
+        else{
+            error.classList.remove('active');
+        }
+
+        error = document.getElementById('daysError');
+
+        if (!days) {    
+
+            error.classList.add('active');
+            return;;
+        }
+        else {
+            error.classList.remove('active');
+        }
+
+        error = document.getElementById('localeError');
+
+        if (!locale) {           
+
+            error.classList.add('active');
+            return;;
+        }
+        else {
+            error.classList.remove('active');
+        }
+
         const calendar = new Calendar(moment(date), Number.parseInt(days), locale);
         calendar.init();
     }, false);
